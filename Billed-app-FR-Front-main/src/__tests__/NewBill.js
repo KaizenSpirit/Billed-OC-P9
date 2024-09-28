6e842c5a6132e46f3348d98cc1e595db1a5ae52c
Anthony Grayling <anthony.grayling.milliondoll1216@hotmail.fr>
2024-09-28 20:18:07 +0200

    Setup initial testing environment and add basic UI test for NewBill page

--- Billed-app-FR-Front-main/src/__tests__/NewBill.js
+++ Billed-app-FR-Front-main/src/__tests__/NewBill.js
@@ -2,17 +2,18 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+// imports nécessaires
+import { screen } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
 
+// Description du premier test
+describe("Given I am connected as an employee and on NewBill Page", () => {
+  test("Then the NewBill page should be displayed with the correct UI", () => {
+    // Définir le contenu du DOM avec le formulaire NewBill
+    document.body.innerHTML = NewBillUI();
+    
+    // Vérification que la page s'affiche avec les bons éléments
+    expect(screen.getByTestId("form-new-bill")).toBeTruthy();
+  });
+});
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
