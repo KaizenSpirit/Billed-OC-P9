913d0cc78d3564b8a2bef667cd216038cebc5dcc
Anthony Grayling <anthony.grayling.milliondoll1216@hotmail.fr>
2024-09-29 13:10:26 +0200

    Add navigation and localStorage mock setup

--- Billed-app-FR-Front-main/src/__tests__/NewBill.js
+++ Billed-app-FR-Front-main/src/__tests__/NewBill.js
@@ -2,17 +2,30 @@
  * @jest-environment jsdom
  */
 
-// imports nécessaires
+import mockStore from "../__mocks__/store";
 import { screen } from "@testing-library/dom";
 import NewBillUI from "../views/NewBillUI.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import { ROUTES } from "../constants/routes";
 
-// Description du premier test
+jest.mock("../app/store", () => mockStore);
+
+const onNavigate = (pathname) => {
+  document.body.innerHTML = ROUTES({ pathname });
+};
+
+beforeEach(() => {
+  Object.defineProperty(window, "localStorage", { value: localStorageMock });
+  window.localStorage.setItem(
+    "user",
+    JSON.stringify({ type: "Employee", email: "a@a" })
+  );
+  global.alert = jest.fn();
+});
+
 describe("Given I am connected as an employee and on NewBill Page", () => {
   test("Then the NewBill page should be displayed with the correct UI", () => {
-    // Définir le contenu du DOM avec le formulaire NewBill
     document.body.innerHTML = NewBillUI();
-    
-    // Vérification que la page s'affiche avec les bons éléments
     expect(screen.getByTestId("form-new-bill")).toBeTruthy();
   });
 });
